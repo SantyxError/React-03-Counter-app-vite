fb1e5049c26d85836c938a072a4fa53dc541713a
santyxerror <sanbemal@gmail.com>
2023-09-05 15:07:17 +0200

    39. Impresión de variables en el HTML

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,14 @@
+
+const getResult = (a, b) => {
+  return a + b
+}
+
+
 export const FirstApp = () => {
   return (
     <>
-    <h2>Por Santiago Benito</h2>
-    <p>Soy un subtítulo</p>
+      <h2>Santiago</h2>
+      <p>Soy un subtítulo</p>
     </>
   )
 }
