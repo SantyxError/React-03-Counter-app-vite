67c9683a24c1ecc94aca6ebe4f907644c5a66592
santyxerror <sanbemal@gmail.com>
2023-09-17 20:11:08 +0200

    43. DefaultProps

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,16 +1,24 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({title, subTitle}) => {
+export const FirstApp = ({title, subTitle, name}) => {
 
   return (
     <>
       <h2>{title}</h2>
       <p>{subTitle}</p>
+      <p>{name}</p>
     </>
   )
 }
 
 FirstApp.propTypes={
 title: PropTypes.string.isRequired,
-subTitle: PropTypes.number.isRequired,
+subTitle: PropTypes.string.isRequired,
+}
+
+FirstApp.defaultProps ={
+  subTitle:"No lo hay, no",
+  name: "Santiago Benito",
+  title: "No hay titulo",
+
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,6 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelloWorldApp />
-    <FirstApp title="Hola, Soy Goku" subTitle={123} />
+    <FirstApp />
   </React.StrictMode>
 )
