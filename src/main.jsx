9f0e679c10f965df9270745c85c0eb53a053886c
santyxerror <sanbemal@gmail.com>
2023-09-18 08:15:27 +0200

    44. Tarea - Componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { HelloWorldApp } from './HelloWorldApp'
 import { FirstApp } from './FirstApp'
 import './styles.css'
+import { CounterApp } from './CounterApp'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorldApp />
-    <FirstApp />
+    {/* <HelloWorldApp /> */}
+    <CounterApp value={10} />
   </React.StrictMode>
 )
