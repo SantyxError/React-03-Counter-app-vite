e293a3f51a1e7592a9e94aa57c5baff0a08efaf0
santyxerror <sanbemal@gmail.com>
2023-09-18 08:46:56 +0200

    46. useState - Hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 export const CounterApp = ({ value }) => {
 
-  const handleAdd = (event, newValue) => {
-    console.log(event);
+  const [counter, setCounter] = useState(value)
+
+  const handleAdd = () => {
+    setCounter(counter + 1)
   }
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {value} </h2>
+      <h2> {counter} </h2>
 
       <button onClick={handleAdd}>
         +1
