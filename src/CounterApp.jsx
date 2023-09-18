defb5b57f801e088911fa717f9a1891e4b05804c
santyxerror <sanbemal@gmail.com>
2023-09-18 08:33:44 +0200

    45. Evento click (Eventos en general)

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-export const CounterApp = ({value}) => {
+export const CounterApp = ({ value }) => {
+
+  const handleAdd = (event, newValue) => {
+    console.log(event);
+  }
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2> {value} </h2>
+
+      <button onClick={handleAdd}>
+        +1
+      </button>
     </>
   )
 }
 
 
-CounterApp.propTypes={
+CounterApp.propTypes = {
   value: PropTypes.number.isRequired,
- 
-  }
+
+}
