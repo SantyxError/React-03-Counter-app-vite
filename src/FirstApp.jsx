3fcf5218ec993eab526280c3a95ac614a140eaae
santyxerror <sanbemal@gmail.com>
2023-09-28 16:46:04 +0200

    66. getByTestId y otras props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,23 +1,24 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({title, subTitle, name}) => {
+export const FirstApp = ({ title, subTitle, name }) => {
 
   return (
     <>
-      <h2>{title}</h2>
+      <div data-testid="test-title">{title}</div>
+      <p>{subTitle}</p>
       <p>{subTitle}</p>
       <p>{name}</p>
     </>
   )
 }
 
-FirstApp.propTypes={
-title: PropTypes.string.isRequired,
-subTitle: PropTypes.string.isRequired,
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired,
 }
 
-FirstApp.defaultProps ={
-  subTitle:"No lo hay, no",
+FirstApp.defaultProps = {
+  subTitle: "No lo hay, no",
   name: "Santiago Benito",
   title: "No hay titulo",
 
--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -6,21 +6,37 @@ describe('Pruebas en <FirstApp/>', () => {
 
   test('debe de hacer match con el snapshot', () => {
 
-    const title = 'Hola, Soy Goku'
-    const { container } = render(<FirstApp title={title} />)
-
-    expect(container).toMatchSnapshot()
+    /*     const title = 'Hola, Soy Goku'
+        const { container } = render(<FirstApp title={title} />)
+    
+        expect(container).toMatchSnapshot() */
 
   })
 
-  test('debe de mostrar el titulo en un h1', () => {
+  test('debe de mostrar el titulo en un h2', () => {
 
     const title = 'Hola, Soy Goku'
-    const { container, getByText } = render(<FirstApp title={title} />)
+    const { container, getByText, getByTestId } = render(<FirstApp title={title} />)
     expect(getByText(title)).toBeTruthy()
 
-    const h2 = container.querySelector('h2')
-    expect(h2.innerHTML).toContain(title);
+    /*    const h2 = container.querySelector('h2')
+       expect(h2.innerHTML).toContain(title); */
+
+    expect(getByTestId('test-title').innerHTML).toContain(title)
+
+  })
+
+  test('debe de mostrar el subtitulo enviado por props', () => {
+
+    const title = 'Hola, Soy Goku'
+    const subtitle = 'Hola, Soy un subtitulo'
+    const { getAllByText } = render(
+      <FirstApp
+        title={title}
+        subTitle={subtitle}
+      />
+    )
+    expect(getAllByText(subtitle).length).toBe(2)
 
   })
 })
